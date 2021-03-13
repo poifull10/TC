60a4ea16cb747f2dc23df2e3f2088b36dc825461
poifull10 <poifull10@gmail.com>
2021-03-14 01:43:56 +0900

    Delete matrix vec

--- tests/test_pinhole.cpp
+++ tests/test_pinhole.cpp
@@ -7,10 +7,10 @@ TEST(Pinhole, None) {
       tcalib::PinholeIntrinsic{10, 10, 10, 10, 20, 20},
       tcalib::distortion::None{});
 
-  std::vector<float> ans_xyz{30.1F, -17.3F, 9.1F};
+  Eigen::Vector3f ans_xyz{30.1F, -17.3F, 9.1F};
   const auto uv = pinholeNone.project(ans_xyz);
   const auto xyz = pinholeNone.unproject(uv);
 
-  ASSERT_FLOAT_EQ(xyz[0] / xyz[2], ans_xyz[0] / ans_xyz[2]);
-  ASSERT_FLOAT_EQ(xyz[1] / xyz[2], ans_xyz[1] / ans_xyz[2]);
+  ASSERT_FLOAT_EQ(xyz(0) / xyz(2), ans_xyz(0) / ans_xyz(2));
+  ASSERT_FLOAT_EQ(xyz(1) / xyz(2), ans_xyz(1) / ans_xyz(2));
 }
