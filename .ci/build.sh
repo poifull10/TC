3e5cac93d19dfa8661ff3244c163f129319e547b
poifull10 <poifull10@gmail.com>
2021-03-14 03:03:16 +0900

    Collect coverage info (#4)
    * Collect coverage info
    
    * Fix gcc
    
    * Add lcov

--- .ci/build.sh
+++ .ci/build.sh
@@ -32,12 +32,14 @@ mkdir -p $build_space && cd $build_space
 
 c_compiler=""
 cxx_compiler=""
+gcov_tool_opt=""
 if [ $compiler_type = "gcc" ]; then
   c_compiler=/usr/local/bin/gcc
   cxx_compiler=/usr/local/bin/g++
 elif [ $compiler_type = "clang" ]; then
   c_compiler=/usr/bin/clang
   cxx_compiler=/usr/bin/clang++
+  gcov_tool_opt="--gcov-tool ../../.ci/llvm-gcov.sh"
 else
   echo "Unexpected option: " $compiler_type
   exit 1
@@ -46,3 +48,9 @@ fi
 cmake ../.. -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler -GNinja
 ninja
 ./tests/TCalibTest
+
+readonly cov_dir=tests/CMakeFiles/TCalibTest.dir
+lcov -d $cov_dir -c -o coverage.info $gcov_tool_opt
+lcov --remove coverage.info '/usr/include/*' '*/gtest/*' '/usr/local/*' '*/gcc/*' -o coverageFiltered.info
+genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
+lcov --list coverageFiltered.info
